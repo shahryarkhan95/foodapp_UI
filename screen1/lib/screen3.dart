41054def033c4912397f522f58cb4f436b519b85
shahryarkhan95 <58082896+shahryarkhan95@users.noreply.github.com>
2021-06-08 21:50:45 +0500

    Screen Three Gradient

--- screen1/lib/screen3.dart
+++ screen1/lib/screen3.dart
@@ -1,24 +1,32 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// import 'package:decorated_text/decorated_text.dart';
-// import 'package:google_fonts/google_fonts.dart';
+import 'package:decorated_text/decorated_text.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-// // import 'package:font_awsome_flutter/font_awsone_flutter.dart';
+// import 'package:font_awsome_flutter/font_awsone_flutter.dart';
 
-// class HomePage extends StatefulWidget {
-//   @override
-//   _HomePageState createState () => HomePageState ();
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-// }
-// class _HomePageState extends State<HomePage> {
-
-//   @override
-
-//   Widget build (BuildContext context) {
-//     return Scaffold (
-//       body: Stack (
-//         fit
-//       )
-//     )
-//   }
-// }
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(fit: StackFit.expand, children: <Widget>[
+      Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color(0xFFFFCC80),
+              const Color(0xFFFFA726),
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+        ),
+      ),
+    ]));
+  }
+}
