7e6731eb34a4f93eb4cab6922526ea9b02bf1358
shahryarkhan95 <58082896+shahryarkhan95@users.noreply.github.com>
2021-06-08 22:15:06 +0500

    font awsome import

--- screen1/lib/screen3.dart
+++ screen1/lib/screen3.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:decorated_text/decorated_text.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-// import 'package:font_awsome_flutter/font_awsone_flutter.dart';
+// import 'package:font_awsome_flutter/font_awsome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   @override
